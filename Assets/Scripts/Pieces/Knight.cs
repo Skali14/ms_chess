a91e3385bfa22e0a7ee4109a335b2ed4be07021e
Michi <89996559+wtseller@users.noreply.github.com>
2025-01-30 18:11:43 +0100

    Added valid move logic for Knight

--- Assets/Scripts/Pieces/Knight.cs
+++ Assets/Scripts/Pieces/Knight.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class Knight : Piece
 {
@@ -8,6 +9,19 @@ public class Knight : Piece
 
     public override bool IsValidMove(int startRow, int startCol, int destRow, int destCol, Piece[,] squares)
     {
-        throw new System.NotImplementedException();
+        if (startRow == destRow && startCol == destCol) return false;
+
+        Piece destPiece = squares[destRow, destCol];
+        if (destPiece != null && destPiece.IsWhite == IsWhite)
+        {
+            return false;
+        }
+
+        //Check if Knight moves in an L shape
+        if (Math.Abs(startRow - destRow) == 1 && Math.Abs(startCol - destCol) == 2 || Math.Abs(startRow - destRow) == 2  && Math.Abs(startCol - destCol) == 1)
+        {
+            return true;
+        }
+        return false;
     }
 }
