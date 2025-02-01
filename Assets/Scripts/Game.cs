46b4af56e629bc5c653abab3f3783eed888e05c6
Michi <89996559+wtseller@users.noreply.github.com>
2025-02-01 11:40:00 +0100

    added method for checking whether King is in check

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -32,6 +32,42 @@ public class Game
         return false;
     }
 
+    private bool isCheck(bool isWhite)
+    {
+        bool foundKing = false;
+        int kingRow = -1;
+        int kingCol = -1;
+
+        for (int i = 0; i < 8 && !foundKing; i++)  // Stop outer loop when found
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Piece piece = board.Squares[i, j];
+                if (piece is King && piece.IsWhite == isWhite)
+                {
+                    kingRow = i;
+                    kingCol = j;
+                    foundKing = true;
+                    break;
+                }
+            }
+        }
+
+        //check if any opponent piece can attack the king
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Piece piece = board.Squares[i, j];
+                if (piece != null && piece.IsWhite != isWhite && piece.IsValidMove(i, j, kingRow, kingCol, board.Squares, this))
+                {
+                    return true; //King is in check
+                }
+            }
+        }
+        return false;
+    }
+
     private bool isStaleMate()
     {
         return false;
