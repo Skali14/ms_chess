37333711d2fec52361fb4ba3ab06fc7e13920de5
fwpop20011 <fwrocket@gmail.com>
2025-02-01 20:43:40 +0100

    Refactor and expand BoardEvalTest with additional tests.
    Renamed test class and methods for better clarity and naming conventions. Added tests for all chess pieces to validate their initial positions on both white and black sides. Improved code formatting and debugging messages for consistency.

--- Assets/Tests/EditMode/BoardEvalTest.cs
+++ Assets/Tests/EditMode/BoardEvalTest.cs
@@ -2,58 +2,59 @@ using NUnit.Framework;
 using UnityEngine;
 
 
-public class NewTestScript
+public class InitBoardTest
 {
-    Chessboard chessboard = new Chessboard();
+    private Chessboard _chessboard = new Chessboard();
 
     [Test]
-    public void testWhiteInitSpaces() {
-        Debug.Log("testing first row initization");
+    public void TestWhiteInitSpaces()
+    {
+        Debug.Log("testing first row initiation");
         //first row 
-        for(char c = 'A'; c <= 'H'; c++)
+        for (char c = 'A'; c <= 'H'; c++)
         {
-            Piece piece = chessboard.getSquare(1, 'A');
+            Piece piece = _chessboard.getSquare(1, 'A');
             bool color = piece.IsWhite;
             Assert.IsTrue(color, $"the color of the (1, {c}) ");
         }
 
-        Debug.Log("testing second row initization");
+        Debug.Log("testing second row initiation");
         //second row
         for (char c = 'A'; c <= 'H'; c++)
         {
-            Piece piece = chessboard.getSquare(2, 'A');
+            Piece piece = _chessboard.getSquare(2, 'A');
             bool color = piece.IsWhite;
             Assert.IsTrue(color);
         }
     }
 
     [Test]
-    public void testBlackInitSpaces()
+    public void TestBlackInitSpaces()
     {
-        Debug.Log("testing seventh row initization");
+        Debug.Log("testing seventh row initiation");
         //seventh row 
         for (char c = 'A'; c <= 'H'; c++)
         {
-            Piece piece = chessboard.getSquare(7, 'A');
+            Piece piece = _chessboard.getSquare(7, 'A');
             bool color = piece.IsWhite;
             Assert.IsTrue(!color);
         }
 
-        Debug.Log("testing eight row initization");
+        Debug.Log("testing eight row initiation");
         //eigth row
         for (char c = 'A'; c <= 'H'; c++)
         {
-            Piece piece = chessboard.getSquare(8, 'A');
+            Piece piece = _chessboard.getSquare(8, 'A');
             bool color = piece.IsWhite;
             Assert.IsTrue(!color);
         }
     }
 
     [Test]
-    public void testInitKingWhite() 
+    public void TestInitKingWhite()
     {
-        Debug.Log("testing white king posistion");
-        Piece king = chessboard.getSquare(1, 'E');
+        Debug.Log("testing white king position");
+        Piece king = _chessboard.getSquare(1, 'E');
         Debug.Log(king.GetType());
         Assert.NotNull(king, "Expected a kind at (1, D) but found null");
         Assert.IsTrue(king is King, "Piece at (1, D) is not King");
@@ -61,13 +62,100 @@ public class NewTestScript
     }
 
     [Test]
-    public void testInitQueenWhite()
+    public void TestInitQueenWhite()
     {
-        Debug.Log("testing white queen posistion");
-        Piece queen = chessboard.getSquare(1, 'D');
+        Debug.Log("testing white queen position");
+        Piece queen = _chessboard.getSquare(1, 'D');
         Debug.Log(queen.GetType());
         Assert.NotNull(queen, "Expected a kind at (1, E) but found null");
         Assert.IsTrue(queen is Queen, "Piece at (1, E) is not queen");
         Assert.IsTrue(queen.IsWhite, "Piece at (1, E) is not white");
+    }
+
+    [Test]
+    public void TestInitQueenBlack()
+    {
+        Debug.Log("testing black queen position");
+        Piece queen = _chessboard.getSquare(8, 'D');
+        Debug.Log(queen.GetType());
+        Assert.NotNull(queen, "Expected a kind at (8, E) but found null");
+        Assert.IsTrue(queen is Queen, "Piece at (8, E) is not queen");
+        Assert.IsTrue(!queen.IsWhite, "Piece at (8, E) is not black");
+    }
+
+    [Test]
+    public void TestInitKingBlack()
+    {
+        Debug.Log("testing black king position");
+        Piece king = _chessboard.getSquare(8, 'E');
+        Debug.Log(king.GetType());
+        Assert.NotNull(king, "Expected a kind at (8, D) but found null");
+        Assert.IsTrue(king is King, "Piece at (8, D) is not King");
+    }
+
+    [Test]
+    public void TestInitRookWhite()
+    {
+        Debug.Log("testing white rook position");
+        Piece rook = _chessboard.getSquare(1, 'A');
+        Debug.Log(rook.GetType());
+        Assert.NotNull(rook, "Expected a kind at (1, A) but found null");
+        Assert.IsTrue(rook is Rook, "Piece at (1, A) is not rook");
+    }
+
+    [Test]
+    public void TestInitRookBlack()
+    {
+        Debug.Log("testing black rook position");
+        Piece rook = _chessboard.getSquare(8, 'A');
+        Debug.Log(rook.GetType());
+        Assert.NotNull(rook, "Expected a kind at (8, A) but found null");
+        Assert.IsTrue(rook is Rook, "Piece at (8, A) is not rook");
+    }
+
+    [Test]
+    public void TestInitBishopWhite()
+    {
+        Debug.Log("testing white bishop position");
+        Piece bishop = _chessboard.getSquare(1, 'C');
+        Debug.Log(bishop.GetType());
+        Assert.NotNull(bishop, "Expected a kind at (1, B) but found null");
+        Assert.IsTrue(bishop is Bishop, "Piece at (1, B) is not bishop");
+    }
+
+    [Test]
+    public void TestInitBishopBlack()
+    {
+        Debug.Log("testing black bishop position");
+        var bishop = _chessboard.getSquare(8, 'C');
+        Debug.Log(bishop.GetType());
+        Assert.NotNull(bishop, "Expected a kind at (8, B) but found null");
+        Assert.IsTrue(bishop is Bishop, "Piece at (8, B) is not bishop");
+    }
+
+    [Test]
+    public void TestInitPawnsWhite()
+    {
+        Debug.Log("testing white pawns position");
+        for (var c = 'A'; c <= 'H'; c++)
+        {
+            var pawn = _chessboard.getSquare(2, c);
+            //Debug.Log(pawn.GetType());
+            Assert.NotNull(pawn, "Expected a kind at (2, {c}) but found null");
+            Assert.IsTrue(pawn is Pawn, "Piece at (2, {c}) is not pawn");
+        }
+    }
+
+    [Test]
+    public void TestInitPawnsBlack()
+    {
+        Debug.Log("testing black pawns position");
+        for (var c = 'A'; c <= 'H'; c++)
+        {
+            var pawn = _chessboard.getSquare(7, c);
+            //Debug.Log(pawn.GetType());
+            Assert.NotNull(pawn, "Expected a kind at (7, {c}) but found null");
+            Assert.IsTrue(pawn is Pawn, "Piece at (7, {c}) is not pawn");
+        }
     }
 }
